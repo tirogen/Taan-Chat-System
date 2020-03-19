d7b15ddb71154d7001a416396af47323fba7666f
NithiP <the-px@hotmail.com>
2020-03-20 01:50:56 +0700

    easy frontend

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import socketIOClient from "socket.io-client";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    socket: socketIOClient('http://localhost:3001'),
+    message: ''
+  }
+
+  componentDidMount() {
+    this.state.socket.on('greet', this.doData)
+  }
+
+  doData = ( msg: string ) => {
+    console.log('come')
+    this.setState({
+      message: msg
+    })
+  }
+
+  handleChange = (e: any) => {
+    this.state.socket.emit('greet', e.target.value)
+  }
+
+  render() {
+    const {message} = this.state;
+    return (
+        <div style={{ textAlign: "center" }}>
+          {message} <br/>
+          <input onChange={this.handleChange} />
+        </div>
+    );
+  }
 }
 
 export default App;
