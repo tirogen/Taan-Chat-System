618b0418a3387ba1f53fff49af65fec35074f160
NithiP <the-px@hotmail.com>
2020-03-19 19:52:27 +0700

    init backend

---
+++ Backend/src/index.ts
@@ -0,0 +1,8 @@
+import express = require('express');
+const app: express.Application = express();
+app.get('/', function (req, res) {
+  res.send('dxHello World!');
+});
+app.listen(3000, function () {
+  console.log('App is listening on port 3000!');
+});
