f3eabd9aef08b2d32747bfec6597513083746613
NithiP <the-px@hotmail.com>
2020-03-20 11:19:15 +0700

    frontend sample

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import socketIOClient from "socket.io-client";
-import './App.css';
+import { Alert } from 'react-bootstrap';
+import './App.scss';
 
 class App extends Component {
 
@@ -10,18 +11,20 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.state.socket.on('greet', this.doData)
-  }
+    this.state.socket.emit('join-room', 'taan0229')
 
-  doData = ( msg: string ) => {
-    console.log('come')
-    this.setState({
-      message: msg
+    this.state.socket.on('display', ({room, message, owner}: Message) => {
+      this.setState({ message })
     })
   }
 
   handleChange = (e: any) => {
-    this.state.socket.emit('greet', e.target.value)
+    const msg: Message = {
+      room: 'taan0229',
+      message: e.target.value,
+      owner: 'uniqueue'
+    }
+    this.state.socket.emit('greet', msg)
   }
 
   render() {
@@ -29,10 +32,28 @@ class App extends Component {
     return (
         <div style={{ textAlign: "center" }}>
           {message} <br/>
-          <input onChange={this.handleChange} />
+          <input onChange={this.handleChange} /><Alert variant="success">
+  <Alert.Heading>Hey, nice to see you</Alert.Heading>
+  <p>
+    Aww yeah, you successfully read this important alert message. This example
+    text is going to run a bit longer so that you can see how spacing within an
+    alert works with this kind of content.
+  </p>
+  <hr />
+  <p className="mb-0">
+    Whenever you need to, be sure to use margin utilities to keep things nice
+    and tidy.
+  </p>
+</Alert>
         </div>
     );
   }
 }
 
 export default App;
+
+interface Message {
+   room: string;
+   message: string;
+   owner: string;
+}
