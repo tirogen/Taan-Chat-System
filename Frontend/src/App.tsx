4a0a08eb6e5a84a673245a9952f9844a0087d870
NithiP <the-px@hotmail.com>
2020-03-20 11:41:07 +0700

    template

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -31,15 +31,6 @@ class App extends Component {
     return (
       <div className="container py-5 px-4">
 
-        <header className="text-center">
-          <h1 className="display-4 text-white">Bootstrap Chat</h1>
-          <p className="text-white lead mb-0">An elegant chat widget compatible with Bootstrap 4</p>
-          <p className="text-white lead mb-4">Snippet by
-            <a href="https://bootstrapious.com" className="text-white">
-              <u>Bootstrapious</u></a>
-          </p>
-        </header>
-
         <div className="row rounded-lg overflow-hidden shadow">
 
           <div className="col-5 px-0">
