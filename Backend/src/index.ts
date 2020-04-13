cad4a6522541154216db8cb79b37623051d8aca6
Thanawat <taan02991@hotmail.com>
2020-04-13 16:08:21 +0700

    Add socketIO redis

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -13,7 +13,9 @@ const PORT: string | undefined = process.env.SOCKET_PORT;
 const app: express.Application = express();
 const server: http.Server = http.createServer(app)
 app.use(cors())
-const io: SocketIO.Server = SocketIO.listen(server, { origins: '*:*' });;
+const io: SocketIO.Server = SocketIO.listen(server, { origins: '*:*' });
+const redisAdapter = require('socket.io-redis');
+io.adapter(redisAdapter({ host: 'redis', port: 6379 }));
 
 server.listen(PORT, () => {
   console.log("Running server on port %s", PORT);
