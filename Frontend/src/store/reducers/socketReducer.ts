09db5cd29000f576f163a188d6b6581b8b4ac408
beesk135 <32613582+beesk135@users.noreply.github.com>
2020-04-14 01:21:15 +0700

    Update socketReducer.ts

--- Frontend/src/store/reducers/socketReducer.ts
+++ Frontend/src/store/reducers/socketReducer.ts
@@ -2,7 +2,7 @@ import { SocketAction, SocketState } from './../type';
 import socketIOClient from "socket.io-client";
 
 const initialState: SocketState = {
-  socket: socketIOClient('http://ip172-18-0-130-bqaa90lim9m000fjl320-3001.direct.labs.play-with-docker.com')
+  socket: socketIOClient('http://ip172-18-0-130-bqaa90lim9m000fjl320-8080.direct.labs.play-with-docker.com')
 }
 
 const socketReducer = (state = initialState, action: SocketAction) => {
