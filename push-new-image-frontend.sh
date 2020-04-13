88fbedfe094dc24abf08b1d27de0229c23090668
beesk135 <32613582+beesk135@users.noreply.github.com>
2020-04-14 01:01:32 +0700

    Update push-new-image-frontend.sh

--- push-new-image-frontend.sh
+++ push-new-image-frontend.sh
@@ -3,3 +3,7 @@ docker-compose build
 docker login
 docker tag taan-chat-system_frontend:lastest bdockbockd/taan-chat-system_frontend:lastest
 docker push bdockbockd/taan-chat-system_frontend:lastest
+docker tag taan-chat-system_backend:lastest bdockbockd/taan-chat-system_backend:lastest
+docker push bdockbockd/taan-chat-system_backend:lastest
+docker tag taan-chat-system_proxy:lastest bdockbockd/taan-chat-system_proxy:lastest
+docker push bdockbockd/taan-chat-system_proxy:lastest
